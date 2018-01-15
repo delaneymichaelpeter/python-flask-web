d7192eebc2643316d6c2ddfbcbc846ccdbc7c22d
Peter Delaney <delaneymichaelpeter@gmail.com>
2018-01-15 08:46:22 -0500

    new values to machine.html

--- python-web.py
+++ python-web.py
@@ -37,25 +37,33 @@ def hello_world():
 
 
 
+@app.route('/ip')
 def get_ec2_ip():
 	url = "http://ifconfig.me"
 	# url = "http://169.254.169.254/latest/meta-data/"
 
 	# IP Address 
 	data = urlopen(url + "public-ipv4").read().decode('utf-8')
-	meta = {'public-ipv4': data}
+	meta = {'public_ipv4': data}
 
 	# Security Group
 	data = urlopen(url + "security-groups").read().decode('utf-8')
-	sg   = {'security-groups': data}
+	sg   = {'security_groups': data}
 
 	# Instance Id
 	data = urlopen(url + "instance-id").read().decode('utf-8')
-	id   = {'instance-id': data}
+	id   = {'instance_id': data}
 
-	return render_template("machine.html", machine=data, sg=sg, id=id)
+	# AMI Id
+	data = urlopen(url + "ami-id").read().decode('utf-8')
+	ami  = {'ami_id': data}
+
+	# Instance Type
+	data = urlopen(url + "instance-type").read().decode('utf-8')
+	type = {'instance_type': data}
+
+	return render_template("machine.html", machine=data, sg=sg, id=id, ami=ami, type=type)
 
 
 if __name__ == '__main__':
-    # app.run(host="localhost", port=5000)
     app.run(host="0.0.0.0", port=5000)
